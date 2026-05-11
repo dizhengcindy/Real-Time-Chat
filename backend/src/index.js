f55449bfec5027d775ba1fa4f2b6776a005d9eb6
dizhengcindy <dizhengcindy@gmail.com>
2026-05-11 11:08:45 -0500

    message model, route, controller

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import { connectDB } from './lib/db.js';
 import authRoutes from './routes/auth.route.js';
+import messageRoutes from './routes/message.route.js';
+
 dotenv.config();
 
 const app = express();
@@ -12,6 +14,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
